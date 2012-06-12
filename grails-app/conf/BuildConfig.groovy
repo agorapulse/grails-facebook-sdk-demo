06195023b3eeaaf0be06727f2568c07c47b23401
Benoit Hediard <hediard@affinitiz.com>
2012-06-12 23:46:53 +0200

    Plugin dependency updated to 0.3.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -42,7 +42,7 @@ grails.project.dependency.resolution = {
         runtime ":resources:1.1.6"
         runtime ":fbootstrapp:0.1.1"
         
-        runtime ':facebook-sdk:0.3.1'
+        runtime ':facebook-sdk:0.3.2'
 
         build ":tomcat:$grailsVersion"
     }
